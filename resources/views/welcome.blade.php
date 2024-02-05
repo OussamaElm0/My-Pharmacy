620935bd014573662e26a31ab6259799ee4271c5
Oussama El Mabrouki <138411649+OussamaElm0@users.noreply.github.com>
2024-02-05 22:32:03 +0100

    remove the register's url from the welcome page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -24,10 +24,6 @@
                         <a href="{{ url('/home') }}" class="font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Home</a>
                     @else
                         <a href="{{ route('login') }}" class="font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Log in</a>
-
-                        @if (Route::has('register'))
-                            <a href="{{ route('register') }}" class="ml-4 font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Register</a>
-                        @endif
                     @endauth
                 </div>
             @endif
