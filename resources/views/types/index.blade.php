4f010321c12eb61cd1b845267047d6e887c99c6d
Oussama El Mabrouki <138411649+OussamaElm0@users.noreply.github.com>
2024-04-09 23:23:53 +0000

    Display all types on table

--- resources/views/types/index.blade.php
+++ resources/views/types/index.blade.php
@@ -6,19 +6,30 @@
         <tr>
             <th class="px-4 py-2 text-left"> # </th>
             <th class="px-4 py-2 text-left"> Name </th>
-            <th class="px-4 py-2 text-left"> Image </th>
-            <th class="px-4 py-2 text-left"> Type </th>
-            <th class="px-4 py-2 text-left"> Category </th>
-            <th class="px-4 py-2 text-left"> Price </th>
-            <livewire:order-quantity />
-            <th class="px-4 py-2 text-left"> Importation Date </th>
-            <th class="px-4 py-2 text-left"> Expiration Date </th>
             <th class="px-4 py-2 text-left">Actions</th>
         </tr>
         </thead>
         <tbody class="divide-y divide-gray-200">
         @foreach($types as $type)
-
+            <tr>
+                <td class="px-4 py-2">{{ $type->id }}</td>
+                <td class="px-4 py-2">{{ $type->name }}</td>
+                <td class="px-4 py-2 flex gap-4">
+                    <a class="btn btn-outline-primary" href="{{ route('types.show', ['type' => $type->id]) }}">
+                        Show
+                    </a>
+                    <a class="btn btn-outline-warning" href="{{ route('types.edit', ['type' => $type->id]) }}">
+                        Edit
+                    </a>
+                    <form method="post" action="{{ route('types.destroy', ['type' => $type->id]) }}">
+                        @csrf
+                        @method('DELETE')
+                        <button type="submit" class="btn btn-outline-danger">
+                            Delete
+                        </button>
+                    </form>
+                </td>
+            </tr>
         @endforeach
         </tbody>
     </table>
