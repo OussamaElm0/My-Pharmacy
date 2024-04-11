508fb0b079a024e83998fa5fcc0293fc3f9470c0
Oussama El Mabrouki <138411649+OussamaElm0@users.noreply.github.com>
2024-04-11 14:46:22 +0000

    Fix model issue in products

--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -95,7 +95,7 @@
                     <td class="text-center">
                         <!-- Button trigger modal -->
                         <button type="button" class="btn btn-outline-dark"
-                                data-bs-toggle="modal" data-bs-target="#actionsModal">
+                                data-bs-toggle="modal" data-bs-target="#product-{{ $product->id }}">
                             <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
                                  class="bi bi-eye" viewBox="0 0 16 16">
                                 <path d="M16 8s-3-5.5-8-5.5S0 8 0 8s3 5.5 8 5.5S16 8 16 8M1.173 8a13 13 0 0 1 1.66-2.043C4.12 4.668 5.88 3.5 8 3.5s3.879 1.168 5.168 2.457A13 13 0 0 1 14.828 8q-.086.13-.195.288c-.335.48-.83 1.12-1.465 1.755C11.879 11.332 10.119 12.5 8 12.5s-3.879-1.168-5.168-2.457A13 13 0 0 1 1.172 8z"/>
@@ -103,12 +103,14 @@
                             </svg>
                         </button>
                         <!-- Modal -->
-                        <div class="modal fade" id="actionsModal" tabindex="-1"
+                        <div class="modal fade" id="product-{{ $product->id }}" tabindex="-1"
                              aria-labelledby="actionsModal" aria-hidden="true">
                             <div class="modal-dialog">
                                 <div class="modal-content">
                                     <div class="modal-header">
-                                        <h1 class="modal-title fs-5" id="exampleModalLabel">{{ $product->name }}</h1>
+                                        <h1 class="modal-title fs-5" id="modalName">
+                                            {{ $product->name }}
+                                        </h1>
                                         <button type="button" class="btn-close"
                                                 data-bs-dismiss="modal" aria-label="Close">
                                         </button>
@@ -116,7 +118,7 @@
                                     <div class="modal-body">
                                         <img src="{{ asset('images/products/' . $product->image) }}" width="200"
                                              height="200" alt="{{ $product->name }}">
-                                        <div class="flex gap-2">
+                                        <div class="flex gap-2 m-1">
                                             <button class="btn btn-outline-primary">
                                                 <a href="{{ route("products.show",['product' => $product->id]) }}">
                                                     Show
