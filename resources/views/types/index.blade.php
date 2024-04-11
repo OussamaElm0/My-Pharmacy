4df444eb9b69068cb42e3eb8e2673969df60555a
Oussama El Mabrouki <138411649+OussamaElm0@users.noreply.github.com>
2024-04-11 14:22:36 +0000

    Styling the create button on types' index page

--- resources/views/types/index.blade.php
+++ resources/views/types/index.blade.php
@@ -6,7 +6,7 @@
             {{ session('success') }}
         </div>
     @endif
-    <a href="{{ route('types.create') }}">Create</a>
+    <a href="{{ route('types.create') }}" class="btn bg-success text-light p-2 m-2">Create</a>
     <table class="table-auto w-full bg-white shadow-md rounded-lg">
         <thead class="bg-gray-50">
         <tr>
