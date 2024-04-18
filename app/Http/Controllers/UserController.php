680f58953e994482d2967ea565cfb05dd57d3974
Oussama El Mabrouki <138411649+OussamaElm0@users.noreply.github.com>
2024-04-18 14:25:39 +0100

    Fix redirection

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -74,7 +74,7 @@ class UserController extends Controller
 
         event(new UserCreatedEvent($user));
 
-        return redirect(RouteServiceProvider::DASHBOARD)->with('success', 'User created succefully');
+        return redirect()->route('users.index')->with('success', 'User created succefully');
     }
 
     /**
