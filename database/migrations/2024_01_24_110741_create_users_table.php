a22cc18daee0096ba8ade3c71f6fcc1bcfed02d9
Oussama El Mabrouki <138411649+OussamaElm0@users.noreply.github.com>
2024-04-18 23:09:33 +0100

    Fix role cannot be null

--- database/migrations/2024_01_24_110741_create_users_table.php
+++ database/migrations/2024_01_24_110741_create_users_table.php
@@ -17,7 +17,6 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->string('cni', 10)->unique();
             $table->foreignId("pharmacy_id")->constrained()->cascadeOnDelete();
-            $table->string('role');
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
